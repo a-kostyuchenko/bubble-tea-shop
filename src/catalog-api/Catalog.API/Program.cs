a229a694470a4b625806aeb9fc4517eacc360e0f
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-07-26 22:06:35 +0200

    Remove redundant comments

--- src/catalog-api/Catalog.API/Program.cs
+++ src/catalog-api/Catalog.API/Program.cs
@@ -1,13 +1,10 @@
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 WebApplication app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
