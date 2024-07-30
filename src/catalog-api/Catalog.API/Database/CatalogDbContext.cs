a631cde1370cee901e4fda34e05ec73bb0d57abc
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-07-30 12:05:58 +0200

    Add database naming convention

--- src/catalog-api/Catalog.API/Database/CatalogDbContext.cs
+++ src/catalog-api/Catalog.API/Database/CatalogDbContext.cs
@@ -4,8 +4,9 @@ namespace Catalog.API.Database;
 
 internal sealed class CatalogDbContext(DbContextOptions<CatalogDbContext> options) : DbContext(options)
 {
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => 
+        optionsBuilder.UseSnakeCaseNamingConvention();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) => 
         modelBuilder.ApplyConfigurationsFromAssembly(AssemblyReference.Assembly);
-    }
 }
