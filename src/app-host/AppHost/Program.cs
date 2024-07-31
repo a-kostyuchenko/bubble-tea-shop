f804da81688c03ae71f6f6843fb27e14353ce299
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-07-31 13:23:44 +0200

    Add WaitFor dependencies method to apphost

--- src/app-host/AppHost/Program.cs
+++ src/app-host/AppHost/Program.cs
@@ -1,12 +1,17 @@
+using AppHost.Extensions;
+using Aspirant.Hosting;
+
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
 IResourceBuilder<PostgresDatabaseResource> catalogDb = builder
     .AddPostgres("postgres")
     .WithDataVolume()
     .WithPgAdmin()
+    .WithHealthCheck()
     .AddDatabase("catalog-db");
 
 builder.AddProject<Projects.Catalog_API>("catalog-api")
-    .WithReference(catalogDb);
+    .WithReference(catalogDb)
+    .WaitFor(catalogDb);
 
 builder.Build().Run();
