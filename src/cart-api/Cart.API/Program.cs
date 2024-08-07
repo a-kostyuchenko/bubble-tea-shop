6960f094ac04a07d50651e1dd3c9ef1ef04e21cc
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-08-07 13:36:59 +0200

    Add Cart.API project

---
+++ src/cart-api/Cart.API/Program.cs
@@ -0,0 +1,35 @@
+using Asp.Versioning;
+using Asp.Versioning.Builder;
+using ServiceDefaults;
+using ServiceDefaults.Endpoints;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => options.CustomSchemaIds(s => s.FullName?.Replace("+", ".")));
+
+builder.AddServiceDefaults();
+
+WebApplication app = builder.Build();
+
+ApiVersionSet apiVersionSet = app.NewApiVersionSet()
+    .HasApiVersion(new ApiVersion(1))
+    .ReportApiVersions()
+    .Build();
+
+RouteGroupBuilder versionedGroup = app
+    .MapGroup("api/v{version:apiVersion}")
+    .WithApiVersionSet(apiVersionSet);
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapDefaultEndpoints();
+app.MapEndpoints(versionedGroup);
+
+app.Run();
