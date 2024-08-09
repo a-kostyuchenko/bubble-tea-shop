78c40a18825d99c5013d4f6a82e3d506078f6576
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-08-09 12:33:22 +0200

    Add Ordering.API

---
+++ src/ordering-api/Ordering.API/Program.cs
@@ -0,0 +1,16 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
