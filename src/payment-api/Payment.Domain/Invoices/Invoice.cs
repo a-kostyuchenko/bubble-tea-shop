6647bba1dc3caa6ad3f48b95fa66399d58e7b39d
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-10-10 14:36:47 +0200

    Add invoice entity

---
+++ src/payment-api/Payment.Domain/Invoices/Invoice.cs
@@ -0,0 +1,19 @@
+using ServiceDefaults.Domain;
+
+namespace Payment.Domain.Invoices;
+
+public sealed class Invoice : Entity
+{
+    private readonly List<InvoiceLine> _lines = [];
+
+    public string Customer { get; set; } = string.Empty;
+    public DateTime IssueTime { get; internal set; }
+    public DateTime? PaymentTime { get; internal set; }
+    public IReadOnlyCollection<InvoiceLine> Lines => [.. _lines];
+    public Money TotalAmount => _lines.Aggregate(Money.Zero(), (total, line) => total + line.Price);
+    
+    public void Add(string label, int quantity, Money price)
+    {
+        _lines.Add(InvoiceLine.Create(Id, label, quantity, price));
+    }
+}
