8073c3f17ff248b84645507b8335475e90785cb4
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-11-22 18:43:29 +0100

    Configure Migration Service

--- src/aspire/MigrationService/Program.cs
+++ src/aspire/MigrationService/Program.cs
@@ -1,6 +1,36 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Cart.API.Infrastructure.Database;
+using Catalog.API.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using MigrationService;
+using Ordering.API.Infrastructure.Database;
+using Payment.Infrastructure.Database;
+using ServiceDefaults;
 
-app.MapGet("/", () => "Hello World!");
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
-app.Run();
+builder.AddServiceDefaults();
+
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracing => tracing.AddSource(Migrator.ActivitySourceName));
+
+builder.Services.AddDbContextPool<CatalogDbContext>((_, options) => 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("catalog-db"))
+        .UseSnakeCaseNamingConvention());
+
+builder.Services.AddDbContextPool<CartDbContext>((_, options) => 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("cart-db"))
+        .UseSnakeCaseNamingConvention());
+
+builder.Services.AddDbContextPool<OrderingDbContext>((_, options) => 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("order-db"))
+        .UseSnakeCaseNamingConvention());
+
+builder.Services.AddDbContextPool<PaymentDbContext>((_, options) => 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("payment-db"))
+        .UseSnakeCaseNamingConvention());
+
+builder.Services.AddHostedService<Migrator>();
+
+IHost app = builder.Build();
+
+await app.RunAsync();
